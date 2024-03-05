4155b3eeb8c2eb479f7e0b81bde8b8cd7b78532a
shard <sh7user@gmail.com>
2024-03-05 20:31:00 +0100

    adding a nix flake for reproductible dev envs

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    fenix.url = "github:nix-community/fenix";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages.rust = {
+                enable = true;
+                channel = "stable";
+                components = [
+                  "rustc"
+                  "cargo"
+                  "clippy"
+                  "rustfmt"
+                  "rust-analyzer"
+                ];
+              };
+
+              # https://devenv.sh/reference/options/
+              packages = [];
+            }
+          ];
+        };
+      });
+  };
+}
