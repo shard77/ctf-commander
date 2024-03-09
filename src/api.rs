dc6f85223ff11c44329f895aa5ad45f56c8ab408
std3 <67806187+standard3@users.noreply.github.com>
2024-03-09 17:00:54 +0100

    del: rename providers to platforms + move into api

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,6 @@
+use reqwest::blocking::{Client, Response};
+use reqwest::{Error, Url};
 use std::fmt;
-
-pub mod providers;
 
 // TODO: querybuilder => POST, GET, ...
 // should include deserializing
@@ -18,5 +18,30 @@ impl fmt::Display for Auth {
             Self::Token(t) => write!(f, "{}", t),
             Self::SessionCookie(sc) => write!(f, "{}", sc),
         }
+    }
+}
+
+pub struct Platform {
+    url: reqwest::Url,
+    auth: Auth,
+    client: Client,
+}
+
+impl Platform {
+    pub fn new(url: Url, auth: Auth) -> Self {
+        Self {
+            url,
+            auth,
+            client: Client::new(),
+        }
+    }
+
+    pub fn get(&mut self, endpoint: String) -> Result<Response, Error> {
+        let target = self.url.join(endpoint.as_str()).unwrap(); // todo: handle error
+
+        self.client
+            .get(target)
+            .bearer_auth(self.auth.clone())
+            .send()
     }
 }
---
+++ src/api/platforms/hackthebox.rs
@@ -0,0 +1,3 @@
+pub struct HackTheBox {
+
+}
