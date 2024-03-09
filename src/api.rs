999c62500ead869129018cfaa6430e98043e0ad4
std3 <67806187+standard3@users.noreply.github.com>
2024-03-09 18:05:06 +0100

    refactor: changed api organization

--- src/api.rs
+++ src/api.rs
@@ -1,18 +1,21 @@
-use reqwest::blocking::{Client, Response};
+use reqwest::blocking::{Client, RequestBuilder, Response};
 use reqwest::{Error, Url};
 use std::fmt;
+use std::time::Duration;
+
+pub mod platforms;
 
 // TODO: querybuilder => POST, GET, ...
 // should include deserializing
 // TODO: add secrets manager (token, creds)
 
 #[derive(Clone)]
-pub enum Auth {
+pub enum AuthMethod {
     Token(String),
     SessionCookie(String),
 }
 
-impl fmt::Display for Auth {
+impl fmt::Display for AuthMethod {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Token(t) => write!(f, "{}", t),
@@ -22,26 +25,40 @@ impl fmt::Display for Auth {
 }
 
 pub struct Platform {
-    url: reqwest::Url,
-    auth: Auth,
     client: Client,
+    base_url: Url,
+    auth_method: AuthMethod,
 }
 
 impl Platform {
-    pub fn new(url: Url, auth: Auth) -> Self {
-        Self {
-            url,
-            auth,
+    pub fn new(base_url: Url, auth_method: AuthMethod) -> Self {
+        Platform {
             client: Client::new(),
+            base_url: base_url,
+            auth_method: auth_method,
         }
     }
 
-    pub fn get(&mut self, endpoint: String) -> Result<Response, Error> {
-        let target = self.url.join(endpoint.as_str()).unwrap(); // todo: handle error
+    fn get_request(&self, endpoint: &str) -> RequestBuilder {
+        let target = self.base_url.join(endpoint).unwrap(); // todo: handle error
 
-        self.client
-            .get(target)
-            .bearer_auth(self.auth.clone())
-            .send()
+        self.client.get(target).timeout(Duration::from_secs(5))
+    }
+
+    fn auth(&self, request: RequestBuilder) -> RequestBuilder {
+        match self.auth_method.clone() {
+            AuthMethod::Token(token) => {
+                request.header("Authorization", format!("Bearer {}", token))
+            }
+            AuthMethod::SessionCookie(cookie) => {
+                todo!()
+            }
+        }
+    }
+
+    pub fn get(&self, endpoint: &str) -> Result<Response, Error> {
+        let request = self.auth(self.get_request(endpoint));
+
+        request.send()
     }
 }
