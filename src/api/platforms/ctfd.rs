809f7ec8f088d823da2a9e862c2dd0cb3384af04
std3 <67806187+standard3@users.noreply.github.com>
2024-04-14 12:18:14 +0200

    feat: added challenge generic + added tags

--- src/api/platforms/ctfd.rs
+++ src/api/platforms/ctfd.rs
@@ -1,13 +1,18 @@
 use crate::api::{AuthMethod, Platform};
+use anyhow::Error;
 use reqwest::Url;
 
-pub use self::types::{challenges, data};
+pub use self::types::{challenges, data, tags};
 
 mod types;
 
 pub struct CTFd {
     platform: Platform,
 }
+
+// TODO: once all types are implemented, change for example `challenge_flags`
+// to return `Result<data::Data<flags::Flag>, Error>` instead of
+// `Result<data::Data<String>, Error>`.
 
 impl CTFd {
     pub fn new(base_url: &str, token: &str) -> CTFd {
@@ -22,9 +27,9 @@ impl CTFd {
     pub fn challenge(
         &self,
         challenge_id: u32,
-    ) -> Result<data::Data<challenges::Challenge>, reqwest::Error> {
-        let endpoint = String::from("challenges") + challenge_id.to_string();
-        self.platform.get(&endpoint)
+    ) -> Result<data::Data<challenges::Challenge>, anyhow::Error> {
+        let endpoint = String::from("challenges/") + challenge_id.to_string();
+        self.platform.get(&endpoint, None)
     }
 
     pub fn challenge_list(
@@ -36,58 +41,102 @@ impl CTFd {
         type_: Option<&str>,
         state: Option<&str>,
         q: Option<&str>,
-    ) -> Result<data::Data<challenges::ChallengeList>, reqwest::Error> {
-        let mut endpoint = String::from("challenges");
+    ) -> Result<data::Data<challenges::ChallengeList>, anyhow::Error> {
+        let mut endpoint = String::from("challenges/");
         let mut query = Vec::new();
 
-        if let Some(name) = name {
-            query.push(format!("name={}", name));
+        macro_rules! add_query_param {
+            ($param:expr, $value:expr) => {
+                if let Some(value) = $value {
+                    query.push(($param, value));
+                }
+            };
         }
 
-        if let Some(max_attempts) = max_attempts {
-            query.push(format!("max_attempts={}", max_attempts));
-        }
+        add_query_param!("name", name);
+        add_query_param!("max_attempts", max_attempts);
+        add_query_param!("value", value);
+        add_query_param!("category", category);
+        add_query_param!("type", type_);
+        add_query_param!("state", state);
+        add_query_param!("q", q);
 
-        if let Some(value) = value {
-            query.push(format!("value={}", value));
-        }
-
-        if let Some(category) = category {
-            query.push(format!("category={}", category));
-        }
-
-        if let Some(type_) = type_ {
-            query.push(format!("type={}", type_));
-        }
-
-        if let Some(state) = state {
-            query.push(format!("state={}", state));
-        }
-
-        if let Some(q) = q {
-            query.push(format!("q={}", q));
-        }
-
-        if !query.is_empty() {
-            endpoint.push_str("?");
-            endpoint.push_str(&query.join("&"));
-        }
-
-        self.platform.get(&endpoint)
+        self.platform.get(&endpoint, Some(&query))
     }
 
-    pub fn challenge_types(&self, challenge_id: u32) -> Result<data::Data<String>, reqwest::Error> {
-        let endpoint = String::from("challenges/types?challenge_id=") + challenge_id.to_string();
-        self.platform.get(&endpoint)
+    fn challenge_generic(
+        &self,
+        endpoint: &str,
+        challenge_id: u32,
+    ) -> Result<data::Data<String>, anyhow::Error> {
+        let endpoint = String::from("challenges/") + endpoint.to_string();
+        self.platform.get(
+            &endpoint,
+            Some(&vec![("challenge_id", challenge_id.to_string())]),
+        )
     }
 
-    pub fn challenge_files(&self, challenge_id: u32) -> Result<data::Data<String>, reqwest::Error> {
-        let endpoint = String::from("challenges/files?challenge_id=") + challenge_id.to_string();
-        self.platform.get(&endpoint)
+    pub fn challenge_types(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("types", challenge_id)
     }
 
-    pub fn challenge_flags(&self, challenge_id: u32) -> Result<data::Data<String>, reqwest::Error> {
-        let endpoint = String::from("challenges/flags?challenge_id=") + challenge_id.to_string();
-        self.platform.get(&endpoint)
+    pub fn challenge_files(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("files", challenge_id)
+    }
+
+    pub fn challenge_flags(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("flags", challenge_id)
+    }
+
+    pub fn challenge_hints(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("hints", challenge_id)
+    }
+
+    pub fn challenge_requirements(
+        &self,
+        challenge_id: u32,
+    ) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("requirements", challenge_id)
+    }
+
+    pub fn challenge_solves(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("solves", challenge_id)
+    }
+
+    pub fn challenge_tags(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("tags", challenge_id)
+    }
+
+    pub fn challenge_topics(&self, challenge_id: u32) -> Result<data::Data<String>, anyhow::Error> {
+        self.challenge_generic("topics", challenge_id)
+    }
+
+    pub fn tag(&self, tag_id: u32) -> Result<data::Data<tags::Tag>, anyhow::Error> {
+        let endpoint = String::from("tags/") + tag_id.to_string();
+        self.platform.get(&endpoint, None)
+    }
+
+    pub fn tag_list(
+        &self,
+        challenge_id: Option<u32>,
+        value: Option<&str>,
+        q: Option<&str>,
+    ) -> Result<data::Data<tags::TagList>, anyhow::Error> {
+        let mut endpoint = String::from("tags");
+        let mut query = Vec::new();
+
+        macro_rules! add_query_param {
+            ($param:expr, $value:expr) => {
+                if let Some(value) = $value {
+                    query.push(($param, value));
+                }
+            };
+        }
+
+        add_query_param!("challenge_id", challenge_id);
+        add_query_param!("value", value);
+        add_query_param!("q", q);
+
+        self.platform.get(&endpoint, Some(&query))
     }
 }
